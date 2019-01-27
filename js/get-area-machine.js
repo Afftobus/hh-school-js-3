9900e0b6368698572cd0a8ed9b1579ca6fb15798
Afftobus <filippoff@mail.ru>
2019-01-28 02:16:22 +0300

    Update get-area-machine.js

--- js/get-area-machine.js
+++ js/get-area-machine.js
@@ -132,41 +132,7 @@ const areaMachine = new machine({
                 }
             }
         },
-        responded: {
-            // action, который нужно выполнить при входе в это состояние. Можно задавать массивом, строкой или функцией
-            onEntry: ['onStateEntry', () => {
-                console.log("it`s work")
-            }]
-        },
-        notResponded: {
-            // action, который нужно выполнить при выходе из этого состояния. Можно задавать массивом, строкой или функцией
-            onExit() {
-                console.log('we are leaving notResponded state');
-            },
-            // Блок описания транзакций
-            on: {
-                // Транзакция
-                RESPOND: {
-                    // упрощенный сервис, вызываем при транзакции
-                    service: (event) => {
-                        // Позволяет получить текущий контекст и изменить его
-                        const [context, setContext] = useContext();
-                        // Позволяет получить текущий стейт и изменить его
-                        const [state, setState] = useState();
-                        // Поддерживаются асинхронные действия
-                        window.fetch({method: 'post', data: {resume: event.resume, vacancyId: context.id}}).then(() => {
-                            // меняем состояние
-                            setState('responded');
-                            // Мержим контекст
-                            setContext({completed: true}); // {id: 123, comleted: true}
-                        });
-                    },
-                    // Если не задан сервис, то просто переводим в заданный target, иначе выполняем сервис.
-                    target: 'responded',
-                }
-            }
-        },
-    },
+
     // Раздел описание экшенов
     actions: {
         inputAction: (event) => {
